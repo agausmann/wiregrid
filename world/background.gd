199189a8b8beaa85c27684d2b30708f53ea166b1
Adam Gausmann <agausmann@fastmail.com>
2020-07-30 02:26:10 -0500

    Fix editor precision

--- world/background.gd
+++ world/background.gd
@@ -1,9 +1,5 @@
 extends Sprite
 
-func _init() -> void:
-	# N.B. Can't accurately set 1/16 (0.0625) in Godot editor
-	scale = Vector2.ONE * (1.0 / 16.0)
-
 func _process(_delta: float) -> void:
 	var viewport_transform = get_viewport_transform().affine_inverse()
 	var local_transform = get_transform().affine_inverse()
